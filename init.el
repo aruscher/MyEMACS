556a7af581dfc9ecd70ad91f40a59318ad971530
Andreas Ruscheinski <andreas.ruscheinski@uni-rostock.de>
2016-09-19 20:44:49 +0200

    update

--- init.el
+++ init.el
@@ -22,3 +22,17 @@
 (menu-bar-mode -1)
 ;;; Remove welcome  message
 (setq inhibit-startup-message t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
